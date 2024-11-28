f4d86a018aa79665a977e4f313f3ba03f4b2b2ae
Edun Yusuf <yusufedun19@gmail.com>
2024-11-28 11:38:25 +0100

    refactor: clean up import statements in PredictivoCopier

--- src/components/Dashboard/PredictivoCopier/PredictivoCopier.tsx
+++ src/components/Dashboard/PredictivoCopier/PredictivoCopier.tsx
@@ -1,4 +1,4 @@
-import React, { useState } from 'react';
+import  { useState } from 'react';
 import { useSelector } from 'react-redux';
 import { RootState } from '@/context/store/rootReducer';
 import { AuthLayout } from '@/components/Layout/layout';
