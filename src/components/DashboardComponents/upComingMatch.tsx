4ec87876a3fda53f9ef7abce663a123d9ea97e86
Edun Yusuf <yusufedun19@gmail.com>
2025-01-28 11:25:19 +0100

    changes for events

--- src/components/DashboardComponents/upComingMatch.tsx
+++ src/components/DashboardComponents/upComingMatch.tsx
@@ -3,14 +3,14 @@ import { useEffect, useState } from "react";
 
 export default function UpcomingMatches() {
   const { getUpcomingMatch } = useDashBoardManagement();
-  const [loading,setLoading] = useState(true)
+  const [loading, setLoading] = useState(true)
   const [upcomingMatch, setUpcomingMatch] = useState<any[]>([]);
 
   useEffect(() => {
     (async () => {
       try {
         const response = await getUpcomingMatch();
-        setUpcomingMatch(response[0]); 
+        setUpcomingMatch(response[0]);
       } catch (error) {
         setLoading(false)
         console.error("Failed to fetch matches", error);
@@ -24,7 +24,7 @@ export default function UpcomingMatches() {
     return <div>Loading matches...</div>;
   }
 
-  
+
 
   return (
     <div className="max-h-screen overflow-y-auto p-4 h-96">
@@ -47,41 +47,50 @@ export default function UpcomingMatches() {
           <hr className="border-t-2 py-2 bg-gray-200" />
 
           {/* Match Details */}
-          <div className="flex flex-row space-x-3 mt-4 rounded-lg bg-slate-200 px-4 py-4 border-l-4 border-green-500">
-            {/* Game Start Time */}
-            <div className="text-center flex items-center justify-center">
-              <h1>{match.game_start_time}</h1>
-            </div>
-
-            {/* Teams */}
-            <div className="space-y-5">
-              <div className="flex flex-row space-x-2 items-center">
-                <img
-                  src={match.home_club_logo}
-                  alt={`${match.home_club_name} Logo`}
-                  className="w-6 h-6"
-                />
-                <h1>{match.home_club_name}</h1>
+          <div className="rounded-lg bg-slate-200 px-4 py-4 border-l-4 border-green-500">
+            <div className="flex flex-row space-x-3 mt-4 ">
+              {/* Game Start Time */}
+              <div className="text-center flex items-center justify-center">
+                <h1>{match.game_start_time}</h1>
               </div>
-              <div className="flex flex-row space-x-2 items-center">
-                <img
-                  src={match.away_club_logo}
-                  alt={`${match.away_club_name} Logo`}
-                  className="w-6 h-6"
-                />
-                <h1>{match.away_club_name}</h1>
+
+              {/* Teams */}
+              <div className="space-y-5">
+                <div className="flex flex-row space-x-2 items-center">
+                  <img
+                    src={match.home_club_logo}
+                    alt={`${match.home_club_name} Logo`}
+                    className="w-6 h-6"
+                  />
+                  <h1>{match.home_club_name}</h1>
+                </div>
+                <div className="flex flex-row space-x-2 items-center">
+                  <img
+                    src={match.away_club_logo}
+                    alt={`${match.away_club_name} Logo`}
+                    className="w-6 h-6"
+                  />
+                  <h1>{match.away_club_name}</h1>
+                </div>
+              </div>
+
+              {/* Scores */}
+              <div className="space-y-5">
+                <h1>{match.home_score}</h1>
+                <h1>{match.away_score}</h1>
               </div>
             </div>
 
-            {/* Scores */}
-            <div className="space-y-5">
-              <h1>{match.home_score}</h1>
-              <h1>{match.away_score}</h1>
+            <div className="flex px-16">               
+              <div className="space-y-5 space-x-2 py-3">
+                <span> predictive scores</span>
+                <span>{match.home_score}</span> -
+                <span>{match.away_score}</span>
+              </div>
+
             </div>
 
-            <div className="flex flex-col">
-              predictive scores r
-            </div>
+
           </div>
         </div>
       ))}
